151b9827f98de6a95d129c13e71e2e1a35e7c5f1
Trevor Lane <tlane@simplenexus.com>
2020-02-06 19:46:28 -0700

    test

--- src/Context/authContext.tsx
+++ src/Context/authContext.tsx
@@ -1,8 +1,4 @@
 import { createContext} from "react";
 
-interface IContextProps {
-    authenticatedUserID: string | null;
-    setAuthenticatedUserID: (authenticatedUserID: string) => void;
-}
 
-export const authContext = createContext({authenticatedUserID: null, setAuthenticatedUserID: {}});
+export const authContext = createContext({authenticatedUserID: null, setAuthenticatedUserID: () => null });
