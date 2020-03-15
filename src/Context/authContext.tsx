f34e2972a857dc8cc42889d8bf0dcc822ba8aeee
Trevor Lane <tlane@simplenexus.com>
2020-03-15 13:15:37 -0600

    update authContext

--- src/Context/authContext.tsx
+++ src/Context/authContext.tsx
@@ -3,6 +3,6 @@ import React, { createContext} from "react";
 type authProps = {
     authenticationToken: {token: string, alias: string} | null;
     setAuthenticationToken: React.Dispatch<React.SetStateAction<null>>
- }
+}
 
 export const authContext = createContext<Partial<authProps>>({});
