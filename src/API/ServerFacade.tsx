64cc47d6ee7544d0db034a03c2a4974b3682cd98
Trevor Lane <tlane@simplenexus.com>
2020-03-20 10:18:21 -0600

    +

--- src/API/ServerFacade.tsx
+++ src/API/ServerFacade.tsx
@@ -5,6 +5,8 @@ require('dotenv').config()
 const local = false;
 const URL = local?  'http://localhost:3000/dev':  process.env.REACT_APP_AWS_URL
 console.log('url: ', URL)
+console.log('raau: ',  process.env.REACT_APP_AWS_URL)
+
 
 export default class ServerFacade {
 
@@ -38,6 +40,8 @@ public static signup = async (alias: string, password: string, picture: string) 
 public static signin =  async ( alias: string, password: string ): Promise< {message:string,alias:string, authenticated: boolean, token:string | null } | null> => {
     
     console.log('test2: ', process.env.NODE_ENV);
+    console.log('test3: ', process.env.REACT_APP_TEST);
+
 
 
     return await fetch(`${URL}/signin`,{
